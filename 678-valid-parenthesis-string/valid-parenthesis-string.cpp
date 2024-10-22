b6d47f3a4025db4a0d93fb670f649c09ea6cac2b
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-10-22 13:28:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (60.04%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    int t[101][101];
-    bool solve(int idx,int open,string &s,int n){
-        if(idx==n){
-            return open==0;
-        }
-
-        if(t[idx][open]!=-1){
-            return t[idx][open];
-        }
-
-        bool isValid=false;
-        if(s[idx]=='*'){
-            isValid |= solve(idx+1,open+1,s,n);
-            isValid |= solve(idx+1,open,s,n);
-            if(open>0){
-                isValid |= solve(idx+1,open-1,s,n);
-            }
-        }else if(s[idx]=='('){
-            isValid |= solve(idx+1,open+1,s,n);
-        }else if(open>0){
-            isValid |=solve(idx+1,open-1,s,n);
-        }
-
-        return t[idx][open]=isValid;
-    }
     bool checkValidString(string s) {
         int n=s.length();
-        memset(t,-1,sizeof(t));
-        return solve(0,0,s,n);
+        int open=0,close=0;
+
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='*') open++;
+            else open--;
+
+            if(open<0) return false;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            if(s[i]==')' || s[i]=='*') close++;
+            else close--;
+
+            if(close<0) return false;
+        }
+
+        return true;
     }
 };
