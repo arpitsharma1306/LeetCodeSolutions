e64f7e58cda724a19238c72b7a3ebfbbf7735f59
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-10-22 21:59:49 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (58.05%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,51 +1,26 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-        int n=ratings.size();
+    int candy(std::vector<int>& ratings) {
+        int n = ratings.size();
+        std::vector<int> candies(n, 1);
 
-        if(n==1) return 1;
-
-        vector<int>left(n,1);
-        vector<int>right(n,1);
-        int count=0;
-
-        if(ratings[0]>ratings[1]) left[0]++;
-        for(int i=1;i<n-1;i++){
-            if(ratings[i]>ratings[i-1] || ratings[i]>ratings[i+1]){
-                if(ratings[i]>ratings[i-1] && ratings[i]<=ratings[i+1]){
-                    left[i]+=left[i-1];
-                }else if(ratings[i]>ratings[i+1] && ratings[i]<=ratings[i-1]){
-                    left[i]+=left[i+1];
-                }else{
-                    int num=max(left[i-1],left[i+1]);
-                    left[i]+=num;
-                }
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
             }
         }
-        if(ratings[n-1]>ratings[n-2]) left[n-1]+=left[n-2];
 
-        if(ratings[n-1]>ratings[n-2]) right[n-1]++;
-        for(int i=n-2;i>0;i--){
-            if(ratings[i]>ratings[i-1] || ratings[i]>ratings[i+1]){
-                if(ratings[i]>ratings[i-1] && ratings[i]<=ratings[i+1]){
-                    right[i]+=right[i-1];
-                }else if(ratings[i]>ratings[i+1] && ratings[i]<=ratings[i-1]){
-                    right[i]+=right[i+1];
-                }else{
-                    int num=max(right[i-1],right[i+1]);
-                    right[i]+=num;
-                }
+        for (int i = n - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = std::max(candies[i], candies[i + 1] + 1);
             }
         }
-        if(ratings[0]>ratings[1]) right[0]+=right[1];
 
-        for(int i=0;i<n;i++){
-            int num=max(left[i],right[i]);
-            cout<<left[i]<<" ";
-            count+=num;
+        int totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
         }
 
-        return count;
-
+        return totalCandies;
     }
 };
