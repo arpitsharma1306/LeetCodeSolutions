a1339757c5d23cca1804fae5adb228a800af4937
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-10-25 15:28:20 +0530

    Time: 71 ms (91.21%) | Memory: 53.3 MB (64.84%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        unordered_set<string>st(begin(folder),end(folder));
+        sort(begin(folder),end(folder));
 
         vector<string>result;
 
-        for(string& currFolder:folder){
-            bool isSubFolder=false;
-            string tempFolder=currFolder;
+        result.push_back(folder[0]);
 
-            while(!currFolder.empty()){
-                size_t position=currFolder.find_last_of('/');
-                currFolder=currFolder.substr(0,position);
-
-                if(st.find(currFolder)!=st.end()){
-                    isSubFolder=true;
-                    break;
-                }
+        for(int i=1;i<folder.size();i++){
+            string currFolder = folder[i];
+            string lastFolder = result.back();
+            lastFolder +='/';
+            if(currFolder.find(lastFolder)!=0){
+                result.push_back(currFolder);
             }
-
-            if(!isSubFolder){
-                result.push_back(tempFolder);
-            }
-            
         }
 
         return result;
