96539567e71ef72c0e77e43f1979ce5108534446
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-10-27 12:04:52 +0530

    Time: 4 ms (16.33%) | Memory: 21.3 MB (16.69%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>>result;
-        bool added=false;
-
-
-        for(auto &interval:intervals){
-            if(newInterval[0]>interval[1]){
-                result.push_back(interval);
-            }
-            else if(newInterval[1]<interval[0]){
-                if(!added){
-                    result.push_back(newInterval);
-                    added=true;
+        vector<vector<int>> res;
+        bool added = false;
+        for (auto interval : intervals) {
+            if (interval[1] < newInterval[0]) { // curr is to the left, just add it
+                res.push_back(interval);
+            } else if (interval[0] > newInterval[1]) {  // curr is to the right, add new interval and add interval
+                if (!added) { 
+                    res.push_back(newInterval);
+                    added = true;
                 }
-                result.push_back(interval);
+                res.push_back(interval);
+            } else { // curr and new interval overlap, merge
+                newInterval[0] = min(newInterval[0], interval[0]);
+                newInterval[1] = max(newInterval[1], interval[1]);
             }
-            else{
-                newInterval[0]=min(newInterval[0],interval[0]);
-                newInterval[1]=max(newInterval[1],interval[1]);
-            }
-        
         }
-        if(!added){
-            result.push_back(newInterval);
+        if (!added) { 
+            res.push_back(newInterval);
         }
-
-        return result;
+        return res;
     }
 };
