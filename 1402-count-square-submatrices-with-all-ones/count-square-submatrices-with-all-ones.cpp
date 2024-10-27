28e582f5c2a9a4646eb15968c2314a4efcbae879
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-10-27 16:49:00 +0530

    Time: 8 ms (39.09%) | Memory: 29.1 MB (56.40%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
-    int m,n;
-
-    int solve(vector<vector<int>>& matrix,int i,int j,vector<vector<int>>&t){
-        if(i>=m || j>=n){
-            return 0;
-        }
-
-        if(matrix[i][j]==0){
-            return 0;
-        }
-
-        if(t[i][j]!=-1){
-            return t[i][j];
-        }
-
-        int right=solve(matrix,i,j+1,t);
-        int diagonal=solve(matrix,i+1,j+1,t);
-        int down=solve(matrix,i+1,j,t);
-
-        return t[i][j]=1+min({right,diagonal,down});
-    }
-    
     int countSquares(vector<vector<int>>& matrix) {
-        m=matrix.size();
-        n=matrix[0].size();
+        int m=matrix.size();
+        int n=matrix[0].size();
 
-        vector<vector<int>>t(m+1,vector<int>(n+1,-1));
+        vector<vector<int>>t(m,vector<int>(n,0));
 
         int result=0;
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                result+=solve(matrix,i,j,t);
+
+                if(i==0 || j==0){
+                    t[i][j]=matrix[i][j];
+                }
+                else if(matrix[i][j]==1){
+                    t[i][j]=1+min({t[i-1][j],t[i-1][j-1],t[i][j-1]});
+                }
+                result+=t[i][j];
             }
         }
 
