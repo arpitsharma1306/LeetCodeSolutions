bee22f402a8cc1595ab1da73492dbab6f48a1b3a
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-12-13 11:38:28 +0530

    Time: 132 ms (50.54%) | Memory: 105.1 MB (83.99%) - LeetSync

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long findScore(vector<int>& nums) {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+
+        for(int i=0;i<nums.size();i++){
+            pq.push({nums[i],i});
+        }
+
+        long long score=0;
+
+        while(!pq.empty()){
+            int num=pq.top().first;
+            int idx=pq.top().second;
+            pq.pop();
+            if(nums[idx]!=-1){
+                score+=nums[idx];
+                if(idx>0) nums[idx-1]=-1;
+                if(idx+1<nums.size()) nums[idx+1]=-1;
+            }
+        }
+
+        return score;
+    }
+};
