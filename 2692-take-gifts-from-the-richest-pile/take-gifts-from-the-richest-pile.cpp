c35ea938bad51210aa13fd7ea1eed46cee99264a
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-12-13 10:48:36 +0530

    Time: 3 ms (54.35%) | Memory: 13 MB (89.08%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    int findMax(vector<int>& gifts,int &idx){
-        int maxi=0;
-        for(int i=0;i<gifts.size();i++){
-            if(gifts[i]>maxi){
-                idx=i;
-                maxi=gifts[i];
-            }
-        }
-
-        return maxi;
-    }
     long long pickGifts(vector<int>& gifts, int k) {
-        
-        for(int i=0;i<k;i++){
-            int idx=-1;
-            int maxi=findMax(gifts,idx);
-            int num=sqrt(maxi);
-            gifts[idx]=num;
+        priority_queue<int>pq;
+
+        for(int &x:gifts){
+            pq.push(x);
         }
 
+        for(int i=0;i<k;i++){
+            int num=pq.top();
+            pq.pop();
+            num=sqrt(num);
+            pq.push(num);
+        }
         long long cnt=0;
-
-        for(int &val:gifts){
-            cnt+=val;
+        while(!pq.empty()){
+            cnt+=pq.top();
+            pq.pop();
         }
 
         return cnt;
