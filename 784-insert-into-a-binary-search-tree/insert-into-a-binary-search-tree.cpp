bb3ec8ec8cd91525e71e1981c7196253210599f5
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-12-15 20:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 59.3 MB (18.55%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -6,31 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode*& root,int val){
-        if(root->left==NULL && root->val>val){
-            root->left = new TreeNode(val);
+    void solve(TreeNode*& root, int val) {
+        if (!root) {
+            root = new TreeNode(val);
             return;
         }
 
-        if(root->right==NULL && root->val<val){
-            root->right = new TreeNode(val);
-            return;
-        }
-
-        if(root->val>val) solve(root->left,val);
-        else solve(root->right,val);
+        if (root->val > val)
+            solve(root->left, val);
+        else
+            solve(root->right, val);
     }
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(!root){
-            TreeNode* node = new TreeNode(val);
-            return node;
-        }
-        solve(root,val);
+
+        solve(root, val);
 
         return root;
     }
