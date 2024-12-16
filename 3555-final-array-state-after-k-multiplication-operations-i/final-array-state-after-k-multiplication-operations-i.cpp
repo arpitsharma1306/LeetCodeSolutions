3572aa4824c2a0dd1b0cb2e247b0580cd2dda912
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-12-16 11:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (45.93%) - LeetSync

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        int n=nums.size();
+        while(k--){
+            int mini=nums[0],idx=0;
+            for(int i=0;i<n;i++){
+                if(mini>nums[i]){
+                    mini=nums[i];
+                    idx=i;
+                }
+            }
+            nums[idx]*=multiplier;
+        }
+
+        return nums;
+    }
+};
