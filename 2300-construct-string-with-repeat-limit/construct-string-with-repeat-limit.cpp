d939ddca23b7c6b9a9c3bf2ac1092f6a50573618
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-12-17 17:14:37 +0530

    Time: 26 ms (77.22%) | Memory: 28.3 MB (25.74%) - LeetSync

---
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) {
+        priority_queue<pair<char,int>>pq;
+
+        vector<int>v(26,0);
+        for(char &ch:s){
+            v[ch-'a']++;
+        }
+        
+        int idx=0;
+        for(char ch='a';ch<='z';ch++){
+            if(v[idx]>0){
+                pq.push({ch,v[idx]});
+            }
+            idx++;
+        }
+
+        string str="";
+        while(!pq.empty()){
+            auto cell = pq.top();
+            pq.pop();
+
+            char ch1=cell.first;
+            int freq1=cell.second;
+
+            if(freq1>repeatLimit){
+                str.append(repeatLimit,ch1);
+                freq1-=repeatLimit;
+                if(pq.empty()) break;
+                else{
+                    auto cell2=pq.top();
+                    pq.pop();
+                    char ch2=cell2.first;
+                    int freq2=cell2.second;
+                    str.append(1,ch2);
+                    freq2-=1;
+                    if(freq2>0){
+                        pq.push({ch2,freq2});
+                    }   
+                }
+                pq.push({ch1,freq1});
+            }
+            else{
+                str.append(freq1,ch1);
+            }
+        }
+
+        return str;
+    }
+};
