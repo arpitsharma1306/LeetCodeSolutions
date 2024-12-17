186d06f300e8535f42512034fb05dd1aba7c4b17
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-12-17 19:29:03 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (27.55%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-    if (grid.size() == 1 && grid[0].size() == 1 && grid[0][0] == 0) {
-        return 0;
-    }
-    if (grid.size() == 1 && grid[0] == vector<int>{0, 0, 0, 0}) {
-    return 0;
-}
-
-
+    
         int m=grid.size();
         int n=grid[0].size();
-        //vector<vector<int>>vis(m,(n,0));
         queue<pair<int,int>>q;
+        int countFreshOrange = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                    //vis[i][j]=1;
                     q.push({i,j});
+                }
+                if(grid[i][j]==1){
+                    countFreshOrange++;
                 }
             }
         }
+        if(countFreshOrange==0) return 0;
 
         int time=0;
         vector<vector<int>>dir{{0,1},{1,0},{-1,0},{0,-1}};
