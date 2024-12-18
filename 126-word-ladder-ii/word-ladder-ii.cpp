10a5e99899eccb4c2c1f7e8d64314cc6134200c5
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-12-18 16:50:09 +0530

    Time: 16 ms (45.05%) | Memory: 12.9 MB (79.16%) - LeetSync

---
+++ 126-word-ladder-ii/word-ladder-ii.cpp
@@ -0,0 +1,68 @@
+class Solution {
+    unordered_map<string, int> mp;
+    vector<vector<string>> ans;
+    string b;
+
+private:
+    void dfs(string word, vector<string>& seq) {
+        if (word == b) {
+            reverse(seq.begin(), seq.end());
+            ans.push_back(seq);
+            reverse(seq.begin(), seq.end());
+            return;
+        }
+
+        int steps = mp[word];
+        int sz = word.size();
+        for (int i = 0; i < sz; i++) {
+            char ori = word[i];
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                word[i] = ch;
+                if (mp.find(word) != mp.end() && mp[word] + 1 == steps) {
+                    seq.push_back(word);
+                    dfs(word, seq);
+                    seq.pop_back();
+                }
+            }
+            word[i] = ori;
+        }
+    }
+
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord,
+                                       vector<string>& wordList) {
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        queue<string> q;
+        b = beginWord;
+        q.push(b);
+        mp[b] = 1;
+        int sz = beginWord.size();
+        st.erase(beginWord);
+
+        while (!q.empty()) {
+            string word = q.front();
+            q.pop();
+            int steps = mp[word];
+            for (int i = 0; i < sz; i++) {
+                char original = word[i];
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    word[i] = ch;
+                    if (st.count(word)) {
+                        q.push(word);
+                        st.erase(word);
+                        mp[word] = steps + 1;
+                    }
+                }
+                word[i] = original;
+            }
+        }
+
+        if (mp.find(endWord) != mp.end()) {
+            vector<string> seq;
+            seq.push_back(endWord);
+            dfs(endWord, seq);
+        }
+
+        return ans;
+    }
+};
