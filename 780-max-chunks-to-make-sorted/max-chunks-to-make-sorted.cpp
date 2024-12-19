a6da035a976f163726e599fa92f80ee0e13d35b9
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-12-19 11:27:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (7.32%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        int n = arr.size();
-        int chunks = 0;
-        int max_seen = -1;
-        for (int i = 0; i < n; i++) {
-            max_seen = max(max_seen, arr[i]);
-            if (max_seen == i)
-                chunks++;
+        int n=arr.size();
+        int chunks=1;
+        vector<int>max_arr(n);
+        vector<int>min_arr(n);
+        int maxi=INT_MIN,mini=INT_MAX;
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,arr[i]);
+            mini=min(mini,arr[n-i-1]);
+            max_arr[i]=maxi;
+            min_arr[n-i-1]=mini;
+        }
+
+        for(int i=0;i<n-1;i++){
+            if(max_arr[i]<=min_arr[i+1]) chunks++;
         }
 
         return chunks;
