54ec60f24cb4b7bd97fb0e33a00feeb122ca3b5b
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-12-19 21:05:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (9.05%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int t[101];
-    int solve(vector<int>& nums, int i, int n) {
-        if (i >= n)
-            return 0;
-
-        if (t[i] != -1)
-            return t[i];
-
-        int steal = nums[i] + solve(nums, i + 2, n);
-        int skip = solve(nums, i + 1, n);
-
-        return t[i] = max(steal, skip);
-    }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        memset(t, -1, sizeof(t));
-        return solve(nums, 0, n);
+        int n=nums.size();
+
+        if(n==1) return nums[0];
+
+        vector<int>t(n+1,0); // n+1 because house no. ki starting 1 se h
+        t[1]=nums[0]; //pehla ghar h to max. pehla hi rob karenge
+        for(int i=2;i<=n;i++){
+            int steal=nums[i-1]+t[i-2]; //wo wala house and usse do pehle ghar
+            int skip=t[i-1];            // last wala ghar
+
+            t[i]=max(steal,skip);
+        }
+
+        return t[n];
     }
 };
