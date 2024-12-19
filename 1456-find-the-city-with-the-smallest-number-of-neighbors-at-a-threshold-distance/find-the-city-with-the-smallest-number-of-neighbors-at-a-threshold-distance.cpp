c1fbf51f3a4cfdd18b0673014706fa1b3e7874c8
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-12-19 19:31:26 +0530

    Time: 10 ms (83.17%) | Memory: 16.6 MB (41.01%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -2,36 +2,24 @@ class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
         int k = distanceThreshold;
-        unordered_map<int, vector<pair<int, int>>> adj;
+        vector<vector<int>>SPM(n,vector<int>(n,1e9+7));
+
+        for(int i=0;i<n;i++){
+            SPM[i][i]=0;
+        }
 
         for (auto& vec : edges) {
             int u = vec[0];
             int v = vec[1];
             int w = vec[2];
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w});
+            SPM[u][v]=w;
+            SPM[v][u]=w;
         }
 
-        vector<vector<int>> result(n, vector<int>(n, INT_MAX));
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            pq;
-
-        for (int i = 0; i < n; i++) {
-            pq.push({0, i});
-            result[i][i] = 0;
-            while (!pq.empty()) {
-                int d = pq.top().first;
-                int node = pq.top().second;
-                pq.pop();
-
-                for (auto& it : adj[node]) {
-                    int adjnode = it.first;
-                    int w = it.second;
-                    if (d + w < result[i][adjnode]) {
-                        result[i][adjnode] = d + w;
-                        pq.push({d + w, adjnode});
-                    }
+        for(int via=0;via<n;via++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    SPM[i][j]=min(SPM[i][j],SPM[i][via]+SPM[via][j]);
                 }
             }
         }
@@ -42,7 +30,7 @@ public:
         for (int i = 0; i < n; i++) {
             int city = 0;
             for (int j = 0; j < n; j++) {
-                if (abs(result[i][i] - result[i][j]) <= k)
+                if (abs(SPM[i][i] - SPM[i][j]) <= k)
                     city++;
             }
             if (city <= mini) {
