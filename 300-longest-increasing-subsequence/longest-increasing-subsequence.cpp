67d683a28010479671ae47781371df810a167380
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-12-21 14:22:16 +0530

    Time: 1 ms (82.74%) | Memory: 14.2 MB (46.49%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    int n;
-    int t[2501][2501];
-    int solve(vector<int>&nums,int idx,int parent){
-        if(idx>=n) return 0;
-
-        if(parent!=-1 && t[idx][parent]!=-1){
-            return t[idx][parent];
-        }
-
-        int skip = solve(nums,idx+1,parent);
-        int taken=0;
-        if(parent==-1 || nums[idx]>nums[parent]){
-            taken = solve(nums,idx+1,idx)+1;
-        }
-
-        if(parent!=-1) t[idx][parent]=max(skip,taken);
-
-        return max(skip,taken);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        n=nums.size();
-        memset(t,-1,sizeof(t));
-        return solve(nums,0,-1);
+        int n = nums.size();
+        vector<int> dp;
+
+        for (int num : nums) {
+            auto it = lower_bound(dp.begin(), dp.end(), num);
+            if (it == dp.end()) {
+                dp.push_back(num); // Extend the subsequence
+            } else {
+                *it = num; // Replace the element to maintain the smallest
+                           // possible values
+            }
+        }
+
+        return dp.size();
     }
 };
