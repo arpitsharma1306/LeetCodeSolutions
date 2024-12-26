f5db6653e9c0b82314ab686cc5d2183803f2a7b5
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2024-12-26 13:07:28 +0530

    Time: 757 ms (23.65%) | Memory: 86.5 MB (7.07%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int n;
+    map<string,int>mp;
     int solve(vector<int>&nums,int target, int idx,int currSum){
         if(idx>=n){
             if(currSum==target) return 1;
             else return 0;
         }
+        string key = to_string(idx)+"_"+to_string(currSum);
+        if(mp.count(key)){
+            return mp[key];
+        }
         int plus = solve(nums,target,idx+1,currSum+nums[idx]);
         int minus = solve(nums,target,idx+1,currSum-nums[idx]);
 
-        return plus+minus;
+        return mp[key]=plus+minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         n=nums.size();
