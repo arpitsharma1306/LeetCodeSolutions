10deecad0718dcb3346bdb4333cf3526647f546b
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-01-09 19:58:45 +0530

    Time: 14 ms (21.48%) | Memory: 13.1 MB (35.94%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    int t[501][501];
-    int solve(string &word1,string &word2,int i,int j){
-        if(i>=word1.size()){
-            return word2.size()-j;
-        }
-        if(j>=word2.size()){
-            return word1.size()-i;
-        }
-
-        if(t[i][j]!=-1) return t[i][j];
-
-        if(word1[i]==word2[j]){
-            return t[i][j]=solve(word1,word2,i+1,j+1);
-        }
-
-        int insertion = 1 + solve(word1,word2,i,j+1);
-        int deletion  = 1 + solve(word1,word2,i+1,j);
-        int replaced  = 1 + solve(word1,word2,i+1,j+1);
-
-        return t[i][j] = min({insertion,deletion,replaced});
-    }
     int minDistance(string word1, string word2) {
-        memset(t,-1,sizeof(t));
-        return solve(word1,word2,0,0);
+        int m=word1.length();
+        int n=word2.length();
+        vector<vector<int>>t(m+1,vector<int>(n+1));
+
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 || j==0){
+                    t[i][j]=i+j;
+                }
+                else if(word1[i-1]==word2[j-1]){
+                    t[i][j]=t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = 1 + min({t[i-1][j],t[i][j-1],t[i-1][j-1]});
+                }
+            }
+        }
+
+        return t[m][n];
     }
 };
