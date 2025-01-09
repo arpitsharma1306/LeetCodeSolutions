41ee0bf9b6182ae180a6cf6d209c79d5ff2cdf90
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-01-09 18:09:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (48.10%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,14 +2,25 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) {
         int n=boxes.size();
-        vector<int>ans(n,0);
+        vector<int>answer(n,0);
 
+        int cumValue=0,cumValueSum=0;
+        //  from left -> right cummalative value nikal rhe aur sath mai us cummalative value ko sum bhi kar rhe
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(boxes[j]=='1') ans[i]+=abs(j-i);
-            }
+            answer[i] = cumValueSum;
+            cumValue += boxes[i]=='0' ? 0:1;
+            cumValueSum+=cumValue;
         }
 
-        return ans;
+        cumValue=0,cumValueSum=0;
+        // yahi same right -> left karna h aur answer mai add karte jana h
+        for(int i=n-1;i>=0;i--){
+            answer[i] += cumValueSum;
+            cumValue += boxes[i]=='0' ? 0:1;
+            cumValueSum+=cumValue;
+        }
+
+        return answer;
+
     }
 };
