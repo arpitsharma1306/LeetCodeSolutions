b9712ba67b3aa543cfb9807fde23ae3a873a7b9c
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-01-14 18:04:31 +0530

    Time: 289 ms (5.00%) | Memory: 243 MB (5.26%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -1,45 +1,34 @@
 class Solution {
 public:
-    int t[100001][3];
-    int n;
-    int finder(vector<vector<int>>& events,int start){
-        int l=0;
-        int r=n-1;
-        int result=n;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(events[mid][0]>start){
-                result=mid;
-                r=mid-1;
-            }else{
-                l=mid+1;
+    //line-sweep method
+    int maxTwoEvents(vector<vector<int>>& events) {
+        vector<vector<int>>line_sweep;
+        for(auto& event:events){
+            int start=event[0];
+            int end=event[1];
+            int value=event[2];
+            line_sweep.push_back({start,1,value});
+            line_sweep.push_back({end+1,-1,value});
+        }
+
+        sort(line_sweep.begin(),line_sweep.end());
+
+        int max_val=0;
+        int max_seen=0;
+
+        for(auto& event:line_sweep){
+            int point=event[0];
+            int status=event[1];
+            int val=event[2];
+
+            if(status==1){
+                max_val=max(max_val,max_seen+val);
+            }
+            if(status==-1){
+                max_seen=max(max_seen,val);
             }
         }
 
-        return result;
-    }
-    int solve(vector<vector<int>>& events,int i,int count){
-        if(count==2 || i>=n){
-            return 0;
-        }
-
-        if(t[i][count]!=-1){
-            return t[i][count];
-        }
-
-        int nextValidIdx = finder(events,events[i][1]);
-
-        int taken = events[i][2] + solve(events,nextValidIdx,count+1);
-        int skip  = solve(events,i+1,count);
-
-        return t[i][count] = max(taken,skip);
-    }
-
-    int maxTwoEvents(vector<vector<int>>& events) {
-        n=events.size();
-        memset(t,-1,sizeof(t));
-        sort(begin(events),end(events));
-        int count=0;
-        return solve(events,0,count);
+        return max_val;
     }
 };
