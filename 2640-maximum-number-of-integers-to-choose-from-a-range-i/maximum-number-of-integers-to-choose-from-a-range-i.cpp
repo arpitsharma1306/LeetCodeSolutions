38b424e83efb8b7a7cae0af3c8012130e3563b56
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-01-14 13:03:42 +0530

    Time: 173 ms (58.47%) | Memory: 177.8 MB (50.06%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
-        int sum=0;
-        int count=0;
-        unordered_set<int>st;
-        for(int i=0;i<banned.size();i++){
-            st.insert(banned[i]);
-        }
-        int num=1;
+        // Initialize a set to store banned numbers for quick lookup
+        unordered_set<int> bannedSet(banned.begin(), banned.end());
+        
+        int sum = 0;    // To keep track of the current sum of chosen numbers
+        int count = 0;  // To count the number of valid numbers chosen
 
-        while(num<=n){
-            if(st.find(num)==st.end()){
-                sum+=num;
-                if(sum<=maxSum) count++;
-                else break;
+        // Iterate through numbers from 1 to n
+        for (int num = 1; num <= n; ++num) {
+            // Check if the current number is not banned
+            if (bannedSet.find(num) == bannedSet.end()) {
+                // Check if adding this number exceeds maxSum
+                if (sum + num > maxSum) {
+                    break; // Exit the loop if the sum would exceed maxSum
+                }
+                // Add the current number to the sum and increment the count
+                sum += num;
+                ++count;
             }
-            num++;
         }
 
-        return count;
+        return count; // Return the total count of valid numbers chosen
     }
 };
