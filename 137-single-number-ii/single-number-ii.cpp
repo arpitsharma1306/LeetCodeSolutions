0dedc16393e9312a1af69550f92a4f9c70887f8c
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-01-15 11:46:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (37.63%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         vector<int>count(32,0);
-        for(int num:nums){
-            for(int i=0;i<32;i++){
-                bool i_bit = num & 1<<i;
-                if(i_bit==1) count[i]++;
+        for (int num : nums) {
+            for (int i = 0; i < 32; ++i) {
+                if (num & (1 << i)) {
+                    count[i]++;
+                }
             }
         }
 
-        int ans=0;
-        for(int i=0;i<32;i++){
-            if(count[i]%3!=0){
-                ans+= 1<<i;
+        int ans = 0;
+        // Reconstruct the single number from bits that are not multiples of 3
+        for (int i = 0; i < 32; ++i) {
+            if (count[i] % 3 != 0) {
+                ans |= 1 << i;
             }
         }
 
