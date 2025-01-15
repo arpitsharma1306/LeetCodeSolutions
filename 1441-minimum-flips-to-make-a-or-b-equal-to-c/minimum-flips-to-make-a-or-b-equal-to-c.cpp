5abdc515031a658072cd35cf28ccad644abf390b
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-01-15 11:06:42 +0530

    Time: 3 ms (23.33%) | Memory: 7.8 MB (32.42%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,24 +1,10 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int count = 0;
-        for (int i = 0; i < 32; ++i) {
-            bool a_bit = a & (1 << i);
-            bool b_bit = b & (1 << i);
-            bool c_bit = c & (1 << i);
-
-            if (c_bit) {
-                // c's bit is 1; at least one of a's or b's bits must be 1
-                if (!a_bit && !b_bit) {
-                    count++; // Need to flip one bit to 1
-                }
-            } else {
-                // c's bit is 0; both a's and b's bits must be 0
-                if (a_bit) count++; // Flip a's bit to 0
-                if (b_bit) count++; // Flip b's bit to 0
-            }
-        }
-
-        return count;
+        int xOR=(a|b)^c;
+        int aND=xOR&(a&b);
+        int count1=__builtin_popcount(xOR);
+        int count2=__builtin_popcount(aND);
+        return count1+count2;
     }
 };
