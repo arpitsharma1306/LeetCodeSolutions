2b6f381f6b298e1fea01d609a6f5d0b637180b2e
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-01-15 12:35:36 +0530

    Time: 4 ms (74.72%) | Memory: 126.6 MB (65.72%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         int n=nums.size();
-        vector<int>cumSum(n);
+        int m=queries.size();
+        vector<int>cumSum(n,0);
         cumSum[0]=0;
         for(int i=1;i<n;i++){
             if(nums[i]%2==nums[i-1]%2){
@@ -12,15 +13,13 @@ public:
             }
         }
 
-        vector<bool>result(queries.size());
+        vector<bool>result(m,false);
         int i=0;
         for(auto& query:queries){
             int start=query[0];
             int end=query[1];
             if(cumSum[end]-cumSum[start]==0){
                 result[i]=true;
-            }else{
-                result[i]=false;
             }
             i++;
         }
