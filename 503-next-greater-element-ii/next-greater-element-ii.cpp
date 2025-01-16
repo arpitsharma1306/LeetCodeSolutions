b73af0ecd84c955e6146628357ee49e7f7817a84
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-01-16 10:15:15 +0530

    Time: 2 ms (82.40%) | Memory: 28.4 MB (72.14%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -2,30 +2,17 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
-        vector<int>nge(n,INT_MIN);
+        vector<int>nge(n,-1);
         stack<int>st;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && nums[i]>=st.top()){
+        for(int i=2*n-1;i>=0;i--){
+            while(!st.empty() && nums[i%n]>=st.top()){
                 st.pop();
             }
-            if(!st.empty()) nge[i]=st.top();
-            st.push(nums[i]);
-        }
-        for(int i=0;i<n;i++){
-            if(nge[i]==INT_MIN){
-                for(int j=0;j<i;j++){
-                    if(nums[i]<nums[j]){
-                        nge[i]=nums[j];
-                        break;
-                    }
-                }
+            if(i<n){
+                if(!st.empty()) nge[i]=st.top();
             }
+            st.push(nums[i%n]);
         }
-
-        for(int i=0;i<n;i++){
-            if(nge[i]==INT_MIN) nge[i]=-1;
-        }
-        
 
         return nge;
     }
