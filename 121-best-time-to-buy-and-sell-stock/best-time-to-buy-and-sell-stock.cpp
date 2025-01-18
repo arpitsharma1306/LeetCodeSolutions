cb99cc5fea9fd5ca01bafc8261de0fb557fd078f
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-01-18 16:13:32 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (23.36%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        stack<int>st;
-        vector<int>GRE(n,-1);
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && st.top()<prices[i]){
-                st.pop();
-            }
-            if(!st.empty())GRE[i]=st.top();
-
-            if(st.empty()) st.push(prices[i]); 
-        }
-
-        int maxProfit=0;
-        for(int i=0;i<n;i++){
-            if(GRE[i]!=-1){
-                int profit=GRE[i]-prices[i];
-                maxProfit=max(maxProfit,profit);
+    int maxProfit(std::vector<int>& prices) {
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
             }
         }
-
-        return maxProfit;
+        return profit;
     }
 };
