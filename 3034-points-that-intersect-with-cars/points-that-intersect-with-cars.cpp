74045f8eb473206e311bdc49338093e67f5ca118
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-01-19 11:53:50 +0530

    Time: 0 ms (100.00%) | Memory: 29.3 MB (69.99%) - LeetSync

--- 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.cpp
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int numberOfPoints(vector<vector<int>>& nums) {
-        int n=nums.size();
-        vector<int>parking(102,0);
-        for(auto num:nums){
+        vector<int> parking(101, 0); // Only need up to 100 indices
+        for (const auto& num : nums) {
             parking[num[0]]++;
-            parking[num[1]+1]--;
+            if (num[1] + 1 <= 100) {
+                parking[num[1] + 1]--;
+            }
         }
-        for(int i=1;i<101;i++){
-            parking[i]=parking[i]+parking[i-1];
-        }
-        int count=0;
 
-        for(int i=1;i<=100;i++){
-            if(parking[i]>0) count++;
+        int count = 0, active = 0;
+        for (int i = 1; i <= 100; ++i) {
+            active += parking[i];
+            if (active > 0) {
+                count++;
+            }
         }
 
         return count;
