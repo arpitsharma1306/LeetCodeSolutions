bbeccd741cfdeec07544676c695bcf5054417e54
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-01-22 10:16:28 +0530

    Time: 69 ms (86.41%) | Memory: 105.4 MB (94.54%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>>dirs = {{0,1},{0,-1},{1,0},{-1,0}};
     vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
-        int m=isWater.size();
-        int n=isWater[0].size();
-        vector<vector<int>>result(m,vector<int>(n));
-        vector<vector<bool>>visited(m,vector<bool>(n,false));
-        queue<vector<int>>q;
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(isWater[i][j]==1){
-                    q.push({0,i,j});
-                    visited[i][j]=true;
+        const int r=isWater.size(), c=isWater[0].size();
+        const int d[5]={0, 1, 0, -1, 0};
+        vector<vector<int>> H(r, vector<int>(c, -1));
+        queue<int> q;
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if (isWater[i][j]){
+                    int idx=(i<<10)+j;
+                    H[i][j]=0;
+                    q.push(idx);
                 }
             }
         }
-
-        while(!q.empty()){
-            int sz=q.size();
-            for(int t=0;t<sz;t++){
-                auto node=q.front();
+        for(int h=0; !q.empty(); h++){
+            int qz=q.size();
+            for(int a=0; a<qz; a++){
+                int idx=q.front(), i=idx>>10, j=idx&((1<<10)-1);
                 q.pop();
-                int h=node[0],x=node[1],y=node[2];
-                result[x][y]=h;
-                for(auto dir:dirs){
-                    int x_=x+dir[0],y_=y+dir[1];
-                    if(x_>=0 && y_>=0 && x_<m && y_<n && !visited[x_][y_]){
-                        q.push({h+1,x_,y_});
-                        visited[x_][y_]=true;
-                    }
+                for(int b=0; b<4; b++){
+                    int s=i+d[b], t=j+d[b+1];
+                    if (s<0||s>=r||t<0||t>=c|| H[s][t]!=-1) continue;
+                    q.push((s<<10)+t);
+                    H[s][t]=h+1;
                 }
             }
         }
-
-        return result;
+        return H;
     }
 };
