07321225c83774dad3df6f094433c4e221f042e0
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-01-30 17:55:06 +0530

    Time: 13 ms (47.87%) | Memory: 34.9 MB (66.40%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    bool isShipped(vector<int>& weights, int days,int capacity){
-        int n = weights.size();
-        int time = 1; 
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (sum + weights[i] > capacity) {
+    bool isShipped(vector<int>& weights, int days, int capacity) {
+        int time = 1, sum = 0;
+        for (int weight : weights) {
+            if (sum + weight > capacity) {
                 time++;
                 sum = 0;
             }
-            sum += weights[i];
+            sum += weight;
             if (time > days) return false;
         }
-
         return true;
     }
+
     int shipWithinDays(vector<int>& weights, int days) {
-        int r=accumulate(weights.begin(),weights.end(),0);
-        int l=*max_element(weights.begin(),weights.end());
-        int result=INT_MAX;
-        
-        while(l<=r){
-            long long mid=l+(r-l)/2;
-            if(isShipped(weights,days,mid)){
-                result=mid;
-                r=mid-1;
-            }else{
-                l=mid+1;
+        int l = *max_element(weights.begin(), weights.end()); // Minimum capacity required
+        int r = accumulate(weights.begin(), weights.end(), 0); // Maximum possible capacity
+
+        while (l < r) { // Changed from `<=` to `<` to reduce unnecessary checks
+            int mid = l + (r - l) / 2;
+            if (isShipped(weights, days, mid)) {
+                r = mid; // Instead of storing in `result`, directly modify `r`
+            } else {
+                l = mid + 1;
             }
         }
 
-        return result;
-
+        return l; // The minimum required capacity
     }
 };
