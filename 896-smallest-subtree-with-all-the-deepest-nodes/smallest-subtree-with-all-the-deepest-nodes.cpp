81760432462843792d2e1f240761b3b0c1bcd11a
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-02-06 09:47:56 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (96.67%) - LeetSync

--- 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -11,52 +11,24 @@
  */
 class Solution {
 public:
-    int height;
-    void maxdepth(TreeNode* root,int curr){
-        if(!root) return;
-        height=max(height,curr);
-
-        if(root->left) maxdepth(root->left,curr+1);
-        if(root->right) maxdepth(root->right,curr+1);
-    }
-
-    void maxdepthNodes(TreeNode* root,unordered_map<TreeNode*,int>&mp,int curr){
-        if(!root) return;
-        if(curr==height) mp[root]++;
-
-        if(root->left) maxdepthNodes(root->left,mp,curr+1);
-        if(root->right) maxdepthNodes(root->right,mp,curr+1);
-    }
-
-    TreeNode* findingNode(TreeNode* root,unordered_map<TreeNode*,int>mp){
-        if(!root) return NULL;
-
-        if(mp.find(root)!=mp.end()){
-            return root;
+    TreeNode* ans;
+    int maxh;
+    int solve(TreeNode* root,int curr){
+        if(!root) return 0;
+        int l = solve(root->left,curr+1);
+        int r = solve(root->right,curr+1);
+        if(l==r){
+            if(maxh<=curr+l){
+                ans=root;
+                maxh=curr+l;
+            }
         }
 
-        TreeNode* leftN=findingNode(root->left,mp);
-        TreeNode* rightN=findingNode(root->right,mp);
-
-        if(leftN!=NULL && rightN!=NULL){
-            return root;
-        }
-
-        if(leftN!=NULL) return leftN;
-
-        return rightN;
-        
+        return 1+max(l,r);
     }
-
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        height = 0;
-        maxdepth(root,0);
-
-        unordered_map<TreeNode*,int>mp;
-        maxdepthNodes(root,mp,0);
-
-        TreeNode* answer = findingNode(root,mp);
-
-        return answer;
+        maxh=0;
+        solve(root,1);
+        return ans;
     }
 };
