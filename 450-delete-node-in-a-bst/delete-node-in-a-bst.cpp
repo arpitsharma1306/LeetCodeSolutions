646e18dce0f74585b16ddc50d3ecd250bf2e7ade
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-02-10 16:30:28 +0530

    Time: 0 ms (100.00%) | Memory: 34.4 MB (28.77%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -6,39 +6,40 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int findMax(TreeNode* root) {
-        int maxi = root->val;
-        while (root) {
-            maxi = max(maxi, root->val);
-            root = root->right;
+    int findMax(TreeNode* root){
+        int maxi=root->val;
+        while(root){
+            maxi=max(maxi,root->val);
+            root=root->right;
         }
+
         return maxi;
     }
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if (!root)
+        if(!root){
             return NULL;
+        }
 
-        if (root->val < key)
-            root->right = deleteNode(root->right, key);
-        else if (root->val > key)
-            root->left = deleteNode(root->left, key);
-        else {
-            if (root->left && root->right) {
+        if(root->val>key){
+            root->left = deleteNode(root->left,key);
+        }else if(root->val<key){
+            root->right = deleteNode(root->right,key);
+        }else{
+            if(root->left && root->right){
                 int v = findMax(root->left);
-                root->val = v;
-                root->left = deleteNode(root->left, v);
+                root->val=v;
+                root->left = deleteNode(root->left,v);
                 return root;
-            } else if (root->left) {
+            }else if(root->left){
                 return root->left;
-            } else if (root->right) {
+            }else if(root->right){
                 return root->right;
-            } else {
+            }else{
                 return NULL;
             }
         }
