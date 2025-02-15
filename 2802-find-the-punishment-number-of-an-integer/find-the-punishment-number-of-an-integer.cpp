96cf87255873751d4ca16e14bf0bf2ef935d9e86
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-02-15 13:13:02 +0530

    Time: 4 ms (83.12%) | Memory: 7.8 MB (95.72%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+    bool solve(int num,int tar){
+        if(tar<0 || num<tar) return false;
+        if(num==tar) return true;
+        return solve(num/10,tar-num%10) || solve(num/100,tar-num%100) || solve(num/1000,tar-num%1000);
+    }
     int punishmentNumber(int n) {
-        int arr[] = { 1, 9, 10, 36, 45, 55, 82, 91, 99, 100, 235, 297, 369, 370, 379, 414, 657, 675, 703, 756, 792, 909,
-            918, 945, 964, 990, 991, 999, 1000 };
-        int res = 0;
-        for (int i = 0; i < 29; i++) {
-            if (arr[i] <= n) {
-                res += arr[i] * arr[i];
-            } else {
-                break;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            int sq=i*i;
+            if(solve(sq,i)){
+                sum+=sq;
             }
         }
-        return res;
+
+        return sum;
     }
 };
