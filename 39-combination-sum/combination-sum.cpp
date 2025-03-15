4a755d7888e513d121a24bdc283c807ff4861d87
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-03-15 13:02:06 +0530

    Time: 6 ms (34.01%) | Memory: 16.6 MB (36.06%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>>answer;
+    void solve(vector<int>& candidates,int idx,int target,vector<int>output,int sum){
+        if(target==sum){
+            answer.push_back(output);
+            return;
+        }
+        if(target<sum){
+            return;
+        }
+        
+        for(int i=idx;i<candidates.size();i++){
+            if (candidates[i] > target-sum) {
+                break; 
+            }
+            output.push_back(candidates[i]);
+            solve(candidates,i,target,output,sum+candidates[i]);
+            output.pop_back();
+        }
+
+        return;
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(begin(candidates),end(candidates));
+        vector<int>output;
+        solve(candidates,0,target,output,0);
+        
+
+        return answer;
+    }
+};
