87d6ba7534a26a113ea786e22171c26a2aef1c85
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-03-26 13:29:43 +0530

    Time: 54 ms (21.82%) | Memory: 104.5 MB (96.36%) - LeetSync

--- 2290-removing-minimum-number-of-magic-beans/removing-minimum-number-of-magic-beans.cpp
+++ 2290-removing-minimum-number-of-magic-beans/removing-minimum-number-of-magic-beans.cpp
@@ -1,26 +1,26 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     long long minimumRemoval(vector<int>& beans) {
-        const int n = beans.size();
+        int n = beans.size();
         if (n == 1) return 0;
-        
+
         sort(beans.begin(), beans.end());
+
+        long long total_sum = 0;
+        for (int& bean : beans) total_sum += bean;
+
+        long long min_remove = total_sum;
         
-        // Calculate prefix sums
-        vector<long long> prefix(n + 1, 0);
-        for (int i = 0; i < n; ++i) {
-            prefix[i + 1] = prefix[i] + beans[i];
+        for (int i = 0; i < n; i++) {
+            long long keep = (long long) beans[i] * (n - i);
+            min_remove = min(min_remove, total_sum - keep);
         }
-        
-        long long min_remove = LLONG_MAX;
-        
-        for (int i = 0; i < n; ++i) {
-            // All beans before i must be removed completely (sum from 0 to i)
-            // All beans after i must be reduced to beans[i] (sum from i+1 to n-1 minus (n-i-1)*beans[i])
-            long long current_remove = prefix[i] + (prefix[n] - prefix[i + 1] - (long long)(n - i - 1) * beans[i]);
-            min_remove = min(min_remove, current_remove);
-        }
-        
+
         return min_remove;
     }
 };
