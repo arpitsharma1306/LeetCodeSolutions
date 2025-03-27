c1b0be75297258830e8e9878a23471cb9f47676c
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-03-27 11:39:11 +0530

    Time: 32 ms (58.20%) | Memory: 98.6 MB (56.56%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -1,8 +1,14 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int, int> freq; 
+        int n = nums.size();
+
+        unordered_map<int, int> freq;
         int maxFreq = 0, maxElement = nums[0];
 
         for (int num : nums) {
@@ -12,21 +18,15 @@ public:
                 maxElement = num;
             }
         }
-        freq.clear(); 
-        double maxiFreq=0;
-        vector<double>left(n), right(n);
-        for(int i=0;i<n;i++){
-            if(nums[i]==maxElement) maxiFreq++;
-            left[i]=(double)(maxiFreq/(double)(i+1));
-        }
-        maxiFreq=0;
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]==maxElement) maxiFreq++;
-            right[i]=(double)(maxiFreq/(double)(n-i));
-        }
 
-        for(int i=0;i<n-1;i++){
-            if(left[i]>0.5 && right[i+1]>0.5){
+        int leftCount = 0, rightCount = maxFreq;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == maxElement) {
+                leftCount++;
+                rightCount--;
+            }
+
+            if (leftCount * 2 > (i + 1) && rightCount * 2 > (n - i - 1)) {
                 return i;
             }
         }
