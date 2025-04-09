88db7efefe31173ed30f79fcb25e0c2f11210997
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-04-09 17:19:23 +0530

    Time: 10 ms (18.85%) | Memory: 27.5 MB (37.94%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int minimumPairRemoval(vector<int>& nums) {
-        list<int>v(nums.begin(),nums.end());
-        int count=0;
+    int minimumPairRemoval(std::vector<int>& nums) {
+        std::list<int> v(nums.begin(), nums.end());
+        int count = 0;
 
-        while(!is_sorted(v.begin(),v.end())){
+        while (!std::is_sorted(v.begin(), v.end())) {
             auto it1 = v.begin();
-            auto it2 = next(it1);
+            auto it2 = std::next(it1);
 
             auto minIt1 = it1;
             auto minIt2 = it2;
             int minSum = *it1 + *it2;
-            while(it2!=v.end()){
+
+            // Find adjacent pair with minimum sum
+            while (it2 != v.end()) {
                 int sum = *it1 + *it2;
-                if(sum<minSum){
+                if (sum < minSum) {
                     minIt1 = it1;
                     minIt2 = it2;
                     minSum = sum;
@@ -21,11 +23,17 @@ public:
                 ++it1;
                 ++it2;
             }
+
+            // Compute the sum *before* modifying the list
             int sum = *minIt1 + *minIt2;
-             v.erase(minIt2);
-            auto insertPos = v.erase(minIt1);
-            v.insert(insertPos,sum);
-            count++;
+
+            // Erase in correct order: minIt2 first, then minIt1
+            // Because erasing minIt1 would invalidate minIt2
+            v.erase(minIt2);
+            auto insertPos = v.erase(minIt1); // erase minIt1 and get insert position
+            v.insert(insertPos, sum);
+
+            ++count;
         }
 
         return count;
