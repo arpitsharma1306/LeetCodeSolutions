0a31870c22b8c7350118807303f1bb12e9a9bd5a
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-04-20 15:03:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (42.46%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.cpp
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -9,10 +9,6 @@ public:
         int ans=0;
         for(auto& it:mp){
             int num=it.first, freq=it.second;
-            if(num==0){
-                ans+=freq;
-                continue;
-            }
             if(freq>num+1){
                 int a = static_cast<int>(std::ceil(static_cast<double>(freq) / (num+1)));
                 int b = (num+1)*a;
