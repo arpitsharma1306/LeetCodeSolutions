b2c59c2103e5d1ee1483b708fe38d3b91935c9ad
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-05-09 17:20:12 +0530

    Time: 69 ms (5.90%) | Memory: 37.3 MB (10.77%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        int m=moveTime.size(), n=moveTime[0].size();
+        int m = moveTime.size(), n = moveTime[0].size();
 
         vector<vector<int>> dirs = {{0,1},{1,0},{0,-1},{-1,0}};
-        vector<vector<int>>reachedTime(m,vector<int>(n,INT_MAX));
-        reachedTime[0][0] = 0;
-        queue<pair<int,int>>q;
-        q.push({0,0});
+        vector<vector<int>> reachedTime(m, vector<int>(n, INT_MAX));
 
-        while(!q.empty()){
-            int x=q.front().first, y=q.front().second;
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> q;
+        q.push({0, 0, 0}); // {time, row, col}
+
+        while (!q.empty()) {
+            auto node = q.top();
             q.pop();
-            for(auto &dir:dirs){
-                int x_=x+dir[0], y_=y+dir[1];
-                if(x_>=0 && y_>=0 && x_<m && y_<n){
-                    int time = reachedTime[x][y]<moveTime[x_][y_]?moveTime[x_][y_]+1:reachedTime[x][y]+1;
-                    if(time<reachedTime[x_][y_]){
-                        reachedTime[x_][y_]=time;
-                        q.push({x_,y_});
+            int time = node[0], x = node[1], y = node[2];
+
+            // Skip if we have already found a better way to this cell
+            if (time >= reachedTime[x][y]) continue;
+
+            // Update with the current shortest time
+            reachedTime[x][y] = time;
+
+            // If target is reached
+            if (x == m - 1 && y == n - 1) return time;
+
+            for (auto& dir : dirs) {
+                int x_ = x + dir[0], y_ = y + dir[1];
+                if (x_ >= 0 && y_ >= 0 && x_ < m && y_ < n) {
+                    int nextTime = max(moveTime[x_][y_], time) + 1;
+                    if (nextTime < reachedTime[x_][y_]) {
+                        q.push({nextTime, x_, y_});
                     }
                 }
             }
         }
 
-        return reachedTime[m-1][n-1];
-
+        return -1; // If unreachable
     }
 };
