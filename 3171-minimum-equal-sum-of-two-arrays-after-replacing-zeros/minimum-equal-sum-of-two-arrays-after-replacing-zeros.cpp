65032f96633686bd3166dd18ba874d48c0d93b8b
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-05-10 17:02:45 +0530

    Time: 128 ms (65.08%) | Memory: 135.4 MB (50.17%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     long long minSum(vector<int>& nums1, vector<int>& nums2) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(0); cout.tie(0);
+        long long zeroes1 = count(nums1.begin(), nums1.end(), 0);
+        long long zeroes2 = count(nums2.begin(), nums2.end(), 0);
+        long long sum1 = accumulate(nums1.begin(), nums1.end(), 0LL);
+        long long sum2 = accumulate(nums2.begin(), nums2.end(), 0LL);
+
         
-        long long nums1_sum = 0, nums2_sum = 0;
-        long long nums1_zeroes = 0, nums2_zeroes = 0;
-
-        for (int x : nums1) {
-            if (x == 0) {
-                nums1_zeroes++;
-                nums1_sum++; // Replace 0 with 1
-            } else {
-                nums1_sum += x;
-            }
-        }
-
-        for (int x : nums2) {
-            if (x == 0) {
-                nums2_zeroes++;
-                nums2_sum++; // Replace 0 with 1
-            } else {
-                nums2_sum += x;
-            }
-        }
-
-        if ((nums1_zeroes == 0 && nums2_sum > nums1_sum) ||
-            (nums2_zeroes == 0 && nums1_sum > nums2_sum))
-            return -1;
-
-        return max(nums1_sum, nums2_sum);
+        if((zeroes1==0 && sum2+zeroes2>sum1+zeroes1) || (zeroes2==0 && sum1+zeroes1>sum2+zeroes2)) return -1;
+        long long res1 = sum1+zeroes1;
+        long long res2 = sum2+zeroes2;
+        return max(res1,res2);
     }
 };
