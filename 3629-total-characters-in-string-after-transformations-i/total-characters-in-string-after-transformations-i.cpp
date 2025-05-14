aead70c2f1464cc05aa1ec4ec33186a92ffe0e7e
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-05-15 04:07:47 +0530

    Time: 814 ms (30.49%) | Memory: 494.9 MB (10.44%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
-    #define MOD 1000000007
-
+    #define mod 1000000007 
+    int mod_add(int a, int b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
     int lengthAfterTransformations(string s, int t) {
-        vector<int>dp(t+26);
-        for(int i=0;i<26;i++) dp[i]=1;
-        for(int i=26;i<t+26;i++){
-            dp[i] = (dp[i-26] + dp[i-25])%MOD;
+        vector<int>nums(26,0);
+        for(char &ch:s){
+            nums[ch-'a']++;
+        }
+
+        while(t--){
+            vector<int>cnt(26,0);
+            for(int i=0;i<26;i++){
+                if(i==25 && nums[i]>0){
+                    cnt[0] = mod_add(cnt[0],nums[i]);
+                    cnt[1] = mod_add(cnt[1],nums[i]);
+                }else{
+                    if(i<25){
+                        cnt[i+1] = mod_add(cnt[i+1],nums[i]);
+                    }
+                    
+                }
+                
+            }
+
+            for (int i = 0; i < 26; i++) {
+    nums[i] = cnt[i];
+}
         }
 
         int ans=0;
-        for(char ch:s){
-            ans = (ans+dp[ch-'a'+t])%MOD;
-        }
+        for(int i=0;i<26;i++){
+            ans = mod_add(ans,nums[i]);
+            ans=ans%mod;
+        } 
 
         return ans;
     }
