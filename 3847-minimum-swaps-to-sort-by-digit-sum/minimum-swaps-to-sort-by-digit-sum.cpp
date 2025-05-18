08464ba058ce7e6d82598ff3cbe96bb5e986cfec
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-05-18 12:51:49 +0530

    Time: 2015 ms (5.01%) | Memory: 512 MB (5.00%) - LeetSync

--- 3847-minimum-swaps-to-sort-by-digit-sum/minimum-swaps-to-sort-by-digit-sum.cpp
+++ 3847-minimum-swaps-to-sort-by-digit-sum/minimum-swaps-to-sort-by-digit-sum.cpp
@@ -1,62 +1,61 @@
 class Solution {
 public:
+    using P = pair<int, int>;
+
     int digit_sum(int n) {
         int sum = 0;
         while (n) {
-            sum += (n % 10);
+            sum += n % 10;
             n /= 10;
         }
-
         return sum;
     }
-    typedef pair<int, int> P;
+
     int minSwaps(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
+
+        // Build a min-heap based on (digit_sum, number)
         priority_queue<P, vector<P>, greater<P>> pq;
-        for (int& num : nums) {
-            int digits = digit_sum(num);
-            pq.push({digits, num});
+        for (int num : nums) {
+            pq.push({digit_sum(num), num});
         }
-        vector<int> result;
+
+        // Sorted version of nums based on digit sum
+        vector<int> sorted;
         while (!pq.empty()) {
-            int ele = pq.top().second;
+            sorted.push_back(pq.top().second);
             pq.pop();
-            result.push_back(ele);
         }
 
-        std::unordered_map<int, std::vector<int>> valueIndices;
-
-        // Record all positions of each value in B
+        // Map each value in sorted array to its indices using a queue
+        unordered_map<int, queue<int>> valueIndices;
         for (int i = 0; i < n; ++i) {
-            valueIndices[result[i]].push_back(i);
+            valueIndices[sorted[i]].push(i);
         }
 
-        // Build the target positions from A to B
-        std::vector<int> targetPos(n);
-        std::unordered_map<int, int> usedCount;
+        // Create a position mapping from original array to sorted one
+        vector<int> targetPos(n);
         for (int i = 0; i < n; ++i) {
             int val = nums[i];
-            targetPos[i] = valueIndices[val][usedCount[val]++];
+            targetPos[i] = valueIndices[val].front();
+            valueIndices[val].pop();
         }
 
-        // Count cycles in targetPos
-        std::vector<bool> visited(n, false);
+        // Count minimum swaps by detecting cycles
+        vector<bool> visited(n, false);
         int swaps = 0;
 
         for (int i = 0; i < n; ++i) {
-            if (visited[i] || targetPos[i] == i)
-                continue;
+            if (visited[i] || targetPos[i] == i) continue;
 
-            int cycleSize = 0;
-            int j = i;
+            int j = i, cycleSize = 0;
             while (!visited[j]) {
                 visited[j] = true;
                 j = targetPos[j];
                 ++cycleSize;
             }
 
-            if (cycleSize > 1)
-                swaps += (cycleSize - 1);
+            swaps += (cycleSize - 1);
         }
 
         return swaps;
