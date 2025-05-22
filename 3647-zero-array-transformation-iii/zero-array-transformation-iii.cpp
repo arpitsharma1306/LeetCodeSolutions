d257dbdab26a54602e9986879b2ab2e4aa909936
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-05-22 20:56:06 +0530

    Time: 99 ms (85.54%) | Memory: 224.1 MB (85.78%) - LeetSync

--- 3647-zero-array-transformation-iii/zero-array-transformation-iii.cpp
+++ 3647-zero-array-transformation-iii/zero-array-transformation-iii.cpp
@@ -1,55 +1,32 @@
 class Solution {
 public:
     int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        int q_sz = queries.size();
-
-        // Sort queries based on the start index
-        sort(queries.begin(), queries.end());
-
-        // Difference array to track changes
-        vector<int> diff(n + 1, 0);
-
-        // Priority queue to manage queries (max-heap based on end index)
-        priority_queue<int> pq;
-
-        int sum = 0; // Tracks the current sum of the difference array
-        int cnt = 0; // Counts the number of queries used
-        int j = 0;   // Pointer to iterate through queries
-
-        for (int i = 0; i < n; i++) {
-            // Add all queries that start at or before the current index
-            while (j < q_sz && queries[j][0] <= i) {
+        int n=nums.size(), q_sz=queries.size();
+        sort(queries.begin(),queries.end());
+        vector<int>diff(n+1,0);
+        int sum=0, j=0;
+        int cnt=0;
+        priority_queue<int>pq;
+        for(int i=0;i<n;i++){
+            while(j<q_sz && queries[j][0]<=i){
                 pq.push(queries[j][1]);
                 j++;
             }
 
-            // Process the difference array
-            sum += diff[i];
+            while(sum+diff[i]<nums[i]){
+                if(pq.empty()) return -1;
 
-            // While the current sum is less than nums[i], try to use queries
-            while (sum < nums[i]) {
-                if (pq.empty()) {
-                    // No more queries to use
-                    return -1;
-                }
-
-                int end = pq.top();
-                pq.pop();
-
-                if (end >= i) {
-                    // Use this query
+                int a=pq.top();
+                pq.pop(); 
+                if(a>=i){
                     diff[i]++;
-                    if (end + 1 < n) {
-                        diff[end + 1]--;
-                    }
-                    sum++;
+                    diff[a+1]--;
                     cnt++;
                 }
             }
+            sum+=diff[i];
         }
 
-        // Return the number of unused queries
-        return q_sz - cnt;
+        return q_sz-cnt;
     }
 };
