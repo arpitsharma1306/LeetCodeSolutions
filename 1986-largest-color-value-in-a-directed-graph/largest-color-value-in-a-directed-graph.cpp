b2c288472ab8121de1fc88b321aa1327fcfa8280
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-05-26 16:04:54 +0530

    Time: 425 ms (30.12%) | Memory: 194.1 MB (22.59%) - LeetSync

--- 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.cpp
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
     int largestPathValue(string colors, vector<vector<int>>& edges) {
-        int n = colors.size();
-        vector<vector<int>> adj(n);
-        vector<int> indegree(n, 0);
-
-        for (auto& edge : edges) {
+        int n=colors.size();
+        unordered_map<int,vector<int>>adj;
+        vector<int>indegree(n,0);
+        for(auto &edge:edges){
             adj[edge[0]].push_back(edge[1]);
             indegree[edge[1]]++;
         }
-
-        queue<int> q;
-        vector<vector<int>> count(n, vector<int>(26, 0)); // count[node][color]
-        
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
+        queue<int>q;
+        vector<vector<int>>t(n,vector<int>(26,0));
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
                 q.push(i);
-                count[i][colors[i] - 'a'] = 1;
+                t[i][colors[i]-'a']=1;
+            } 
+        }
+
+        int countNodes=0,result=0;
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            countNodes++;
+            result=max(result,t[u][colors[u]-'a']);
+
+            for(auto& v:adj[u]){
+                for(int i=0;i<26;i++){
+                    t[v][i] = max(t[v][i], t[u][i] + (i==colors[v]-'a'));
+                }
+                indegree[v]--;
+                if(indegree[v]==0){
+                    q.push(v);
+                }
             }
         }
 
-        int maxColor = 0;
-        int visited = 0;
+        if(countNodes<n) return -1;
 
-        while (!q.empty()) {
-            int node = q.front(); q.pop();
-            visited++;
-
-            for (int nei : adj[node]) {
-                for (int c = 0; c < 26; c++) {
-                    count[nei][c] = max(count[nei][c], count[node][c] + (colors[nei] - 'a' == c));
-                }
-                indegree[nei]--;
-                if (indegree[nei] == 0) {
-                    q.push(nei);
-                }
-            }
-
-            maxColor = max(maxColor, *max_element(count[node].begin(), count[node].end()));
-        }
-
-        return visited == n ? maxColor : -1; // if not all nodes visited → cycle exists
+        return result;
     }
 };
