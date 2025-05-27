d01b5675dcf9c62152289abde75c9e10389f0dc4
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-05-27 10:46:30 +0530

    Time: 270 ms (26.18%) | Memory: 91.6 MB (28.90%) - LeetSync

--- 3857-find-maximum-number-of-non-intersecting-substrings/find-maximum-number-of-non-intersecting-substrings.cpp
+++ 3857-find-maximum-number-of-non-intersecting-substrings/find-maximum-number-of-non-intersecting-substrings.cpp
@@ -2,37 +2,30 @@ class Solution {
 public:
     int maxSubstrings(string word) {
         int n = word.size();
-        if (n < 4)
-            return 0;
-        int cnt = 0;
-        unordered_set<char> st;
-        unordered_map<char, int> mp;
-        st.insert(word[0]);
-        mp[word[1]] = 2;
-        if(word[1]!=word[2]) mp[word[2]] = 1;
+        vector<vector<int>> mpp(26);
+        for (int i = 0; i < n; i++) 
+            mpp[word[i] - 'a'].push_back(i);
 
-        for (int i = 3; i < n; i++) {
-            if (st.find(word[i]) != st.end()) {
-                cnt++;
-                st.clear();
-                mp.clear();
-                continue;
-            }
-
-            for (auto it = mp.begin(); it != mp.end();) {
-                it->second += 1;
-                if (it->second == 3) {
-                    st.insert(it->first);
-                    it = mp.erase(it); // returns iterator to next element
-                } else {
-                    ++it;
+        vector<pair<int, int>> intervals;
+        for (int i = 0; i < 26; i++) {
+            auto &curr = mpp[i];
+            for (int j = 0; j < curr.size(); j++) {
+                auto it = lower_bound(curr.begin() + j + 1, curr.end(), curr[j] + 3);
+                if (it != curr.end()) {
+                    intervals.emplace_back(*it, curr[j]);
                 }
             }
-            if(mp.find(word[i])!=mp.end()) continue;
-
-            mp[word[i]]=1;
         }
 
-        return cnt;
+        sort(intervals.begin(), intervals.end());
+        int count = 0, last = -1;
+        for (auto &p : intervals) {
+            if (p.second > last) {
+                ++count;
+                last = p.first;
+            }
+        }
+
+        return count;
     }
 };
