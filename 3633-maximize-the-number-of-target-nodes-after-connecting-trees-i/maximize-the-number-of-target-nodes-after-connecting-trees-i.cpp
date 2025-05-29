b96fe4c37dc52590835b5bbff99445c7ea561954
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-05-29 13:46:41 +0530

    Time: 119 ms (81.94%) | Memory: 59.1 MB (99.54%) - LeetSync

--- 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -1,56 +1,29 @@
 class Solution {
 public:
-    int bfs(unordered_map<int,vector<int>>&adj,int node,int k){
-        queue<int>q;
-        q.push(node);
-        int depth=1;
-        vector<bool>vis(adj.size(),false);
-        vis[node]=true;
-        int cnt=0;
-        while(depth<=k && !q.empty()){
-            int sz=q.size();
-            depth++;
-            for(int i=0;i<sz;i++){
-                cnt++;
-                int curr=q.front();
-                q.pop();
-                for(auto &child:adj[curr]){
-                    if(!vis[child]){
-                        q.push(child);
-                        vis[child]=true;
-                    }
-                }
-            }
+    vector<vector<int>> buildList(const vector<vector<int>>& edges) {
+        vector<vector<int>> adj(edges.size() + 1);
+        for (auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
-
+        return adj;
+    }
+    
+    int dfs(const vector<vector<int>>& adj, int u, int p, int k) {
+        if (k < 0) return 0;
+        int cnt = 1;
+        for (int v : adj[u])
+            if (v != p) cnt += dfs(adj, v, u, k-1);
         return cnt;
     }
-
+    
     vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
-        int n=edges1.size()+1, m=edges2.size()+1;
-        unordered_map<int,vector<int>>adj1(n);
-        unordered_map<int,vector<int>>adj2(m);
+        auto adj1 = buildList(edges1), adj2 = buildList(edges2);
+        int n = adj1.size(), m = adj2.size(), maxiB = 0;
+        vector<int> res(n);
 
-        for(auto &edge:edges1){
-            adj1[edge[0]].push_back(edge[1]);
-            adj1[edge[1]].push_back(edge[0]);
-        }
-        for(auto &edge:edges2){
-            adj2[edge[0]].push_back(edge[1]);
-            adj2[edge[1]].push_back(edge[0]);
-        }
-        int maxNodes2=0;
-        for(int i=0;i<m;i++){
-            maxNodes2=max(maxNodes2,bfs(adj2,i,k));
-        }
-        cout<<maxNodes2;
-        vector<int>answer(n);
-        for(int i=0;i<n;i++){
-            int nodes=bfs(adj1,i,k+1);
-            answer[i]=nodes+maxNodes2;
-        }
-
-        return answer;
-        
+        for (int i = 0; i < m; i++) maxiB = max(maxiB, dfs(adj2, i, -1, k - 1));
+        for (int i = 0; i < n; i++) res[i] = dfs(adj1, i, -1, k) + maxiB;
+        return res;
     }
 };
