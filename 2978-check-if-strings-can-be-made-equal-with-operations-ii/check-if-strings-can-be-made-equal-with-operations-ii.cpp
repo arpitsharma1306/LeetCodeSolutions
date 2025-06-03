00fb781e6d7ba8fbb5c18db18c9d3f390a9b474b
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-06-03 12:18:17 +0530

    Time: 7 ms (67.72%) | Memory: 20.1 MB (50.00%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     bool checkStrings(string s1, string s2) {
-        int n=s1.size();
-        vector<int>freq1o(26,0), freq1e(26,0), freq2o(26,0), freq2e(26,0);
-
-        for(int i=0;i<n;i+=2){
-            freq1e[s1[i]-'a']++;
-            freq2e[s2[i]-'a']++;
+        vector<int> freq1e(26, 0), freq1o(26, 0), freq2e(26, 0), freq2o(26, 0);
+        for (int i = 0; i < s1.size(); ++i) {
+            if (i % 2 == 0) {
+                freq1e[s1[i] - 'a']++;
+                freq2e[s2[i] - 'a']++;
+            } else {
+                freq1o[s1[i] - 'a']++;
+                freq2o[s2[i] - 'a']++;
+            }
         }
-        for(int i=1;i<n;i+=2){
-            freq1o[s1[i]-'a']++;
-            freq2o[s2[i]-'a']++;
-        }
-
-        if(freq1e==freq2e && freq1o==freq2o) return true;
-
-        return false;
+        return freq1e == freq2e && freq1o == freq2o;
     }
 };
