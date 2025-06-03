55a5d87939f4bd830c9c4b47b6fb23d672776aa7
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-06-03 15:25:39 +0530

    Time: 12 ms (48.24%) | Memory: 18.2 MB (24.71%) - LeetSync

--- 3057-count-k-subsequences-of-a-string-with-maximum-beauty/count-k-subsequences-of-a-string-with-maximum-beauty.cpp
+++ 3057-count-k-subsequences-of-a-string-with-maximum-beauty/count-k-subsequences-of-a-string-with-maximum-beauty.cpp
@@ -1,47 +1,33 @@
 class Solution {
 public:
+    unordered_map<int, int>freq;
+    vector<int>power;
+    int reqLen;
+    unordered_map<string, int>seen;
     int mod = 1e9 + 7;
-    unordered_map<char, int> freq;
-    vector<int> power;
-    unordered_map<string, int> seen;
-    int mx = 0;
-
-    long long solve(int i, int k, int m) {
-        if (i == power.size()) {
-            return (k == 0 && m == 0) ? 1 : 0;
+    int dp[26][26];
+    long long int solve( int i, int k){
+        if(i == power.size()){
+            return k == reqLen;
         }
-        if (k < 0 || m < 0) return 0;
-
-        string key = to_string(i) + "#" + to_string(k) + "#" + to_string(m);
-        if (seen.count(key)) return seen[key];
-
-        // Include current power
-        long long take = (power[i] * solve(i + 1, k - 1, m - power[i])) % mod;
-
-        // Exclude current power
-        long long notake = solve(i + 1, k, m);
-
-        return seen[key] = (take + notake) % mod;
+        if(k > reqLen)return 0;
+        if(dp[i][k] != -1)return dp[i][k];
+        long long int take = 0, notake = 0;
+        if(power[i] == power[k])
+        take = power[i] * solve(i + 1, k + 1);
+        notake = solve( i + 1, k);
+        return dp[i][k] = (take + notake) % mod;
     }
-
     int countKSubsequencesWithMaxBeauty(string s, int k) {
-        for (char c : s) {
-            freq[c]++;
+        int n = s.size();
+        for(auto c: s)freq[c]++;
+        reqLen = k;
+        memset(dp, -1, sizeof(dp));
+        if(k > freq.size() || k > 26)return 0;
+        for(auto m: freq){
+            power.push_back(m.second);
         }
-
-        if (k > freq.size() || k > 26) return 0;
-
-        for (auto& entry : freq) {
-            power.push_back(entry.second);
-        }
-
-        sort(power.rbegin(), power.rend());  // Sort descending
-
-        for (int i = 0; i < k; ++i) {
-            if (i >= power.size()) return 0;
-            mx += power[i];
-        }
-
-        return solve(0, k, mx);
+        sort(power.rbegin(), power.rend());
+        return solve( 0, 0); 
     }
 };
