52583d5ee9025ffcf2fcb166ac5d8a288f8d4790
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-06-04 15:07:26 +0530

    Time: 1329 ms (43.96%) | Memory: 134.7 MB (40.66%) - LeetSync

--- 2953-count-pairs-of-points-with-distance-k/count-pairs-of-points-with-distance-k.cpp
+++ 2953-count-pairs-of-points-with-distance-k/count-pairs-of-points-with-distance-k.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int countPairs(vector<vector<int>>& c, int k) {
-        int n=c.size();
-        int pairs = 0;
-        unordered_map<long long,long long>mp;
-        for(int i=0;i<n;i++){
-            for(int val=0;val<=k;val++){
-                long long x = val^c[i][0];
-                long long y = (k-val)^c[i][1];
-                long long key = x*1000001+y;
-                if(mp.count(key)){
-                    pairs+=mp[key];
+        map<pair<int, int>, int> f;
+        long long ans = 0;
+        for(auto i: c) {
+            for(int j = 0; j <= k; j++) {
+                int x = j ^ i[0];
+                int y = (k - j) ^ i[1];
+                if(f.find({x, y}) != f.end()) {
+                    ans += f[{x, y}];
                 }
             }
-            long long outer_key = (long long)c[i][0]*(long long)1000001+(long long)c[i][1];
-            mp[outer_key]++;
+            f[{i[0], i[1]}]++;
         }
-
-        return pairs;
+        return ans;
     }
 };
