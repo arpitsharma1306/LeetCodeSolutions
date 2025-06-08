a5995f021320d262e86c4997ad74aa524740cc5e
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-06-08 10:09:45 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (69.54%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    void dfs(vector<int>&ans, int num,int n){
-        string s = to_string(num);
-        for(int i=0;i<10;i++){
-            string s1 = s + to_string(i);
-            int num1 = stoi(s1);
-            if(num1<=n){
-                ans.push_back(num1);
-                dfs(ans,num1,n);
-            }
+    void dfs(vector<int>& ans, int curr, int n) {
+        if (curr > n) return;
+        ans.push_back(curr);
+        for (int i = 0; i < 10; ++i) {
+            int next = curr * 10 + i;
+            if (next > n) break;
+            dfs(ans, next, n);
         }
     }
-    vector<int> lexicalOrder(int n) {
-        vector<int>ans;
-        for(int i=1;i<10;i++){
-            if(i<=n){
-                ans.push_back(i);
-                dfs(ans,i,n);
-            }
-        }
 
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        for (int i = 1; i <= 9; ++i) {
+            if (i > n) break;
+            dfs(ans, i, n);
+        }
         return ans;
     }
 };
