f39848c7c1ef8009027dffc3f65e59559ceb07dd
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-06-14 17:50:49 +0530

    Time: 26 ms (60.12%) | Memory: 86.9 MB (17.36%) - LeetSync

--- 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
-    int minimizeMax(vector<int>& nums, int p) {
-        if (p == 0) return 0;
-        sort(nums.begin(), nums.end());
-        int n = nums.size(), left = 0, right = nums[n - 1] - nums[0];
-
-        while (left < right) {
-            int mid = left + (right - left) / 2, pairs = 0;
-            for (int i = 1; i < n; ++i) {
-                if (mid >= nums[i] - nums[i - 1]) {
-                    ++pairs, ++i;
-                }
+    bool check(vector<int>& nums, int p,int maxi){
+        int pairs = 0;
+        for(int i=0;i<nums.size()-1;){
+            if(pairs>=p) return true;
+            if(nums[i+1]-nums[i]<=maxi){
+                pairs++;
+                i+=2;
+            }else{
+                i++;
             }
-            if (pairs >= p) right = mid;
-            else left = mid + 1;
         }
-        return left;
+        if(pairs>=p) return true;
+
+        return false;
+    }
+    int minimizeMax(vector<int>& nums, int p) {
+        sort(nums.begin(),nums.end());
+        int right = nums.back() - nums[0];
+        int left = 0;
+        int ans = right;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(check(nums,p,mid)){
+                ans = mid;
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+
+        return ans;
     }
 };
