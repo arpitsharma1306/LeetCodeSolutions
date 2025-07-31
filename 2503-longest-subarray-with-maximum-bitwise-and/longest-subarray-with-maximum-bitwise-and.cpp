7dbd23df07132573bb174e9d5c393e38836af356
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-08-01 00:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 86.2 MB (8.82%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,35 +1,27 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int longestSubarray(std::vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
+    int longestSubarray(vector<int>& nums) {
+        int n=nums.size();
+        int size=1;
+        int maxi=nums[0];
+        int curr_size=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]==maxi && nums[i]==nums[i-1]){
+                curr_size++;
+                size=max(size,curr_size);
+                continue;
+            }
+            if(maxi<nums[i]){
+                maxi=nums[i];
+                curr_size=1;
+                size=1;
+                continue;
+            }
+            if(nums[i]==maxi){
+                curr_size=1;
+            }
         }
 
-        // 1. Find the maximum value in the array.
-        int maxVal = 0;
-        for (int num : nums) {
-            if (num > maxVal) {
-                maxVal = num;
-            }
-        }
-        
-        // 2. Find the length of the longest subarray of maxVal.
-        int maxLen = 0;
-        int currentLen = 0;
-        
-        for (int num : nums) {
-            if (num == maxVal) {
-                currentLen++;
-            } else {
-                maxLen = std::max(maxLen, currentLen);
-                currentLen = 0;
-            }
-        }
-        
-        // 3. Final check for a trailing streak.
-        return std::max(maxLen, currentLen);
+        return size;
     }
 };
