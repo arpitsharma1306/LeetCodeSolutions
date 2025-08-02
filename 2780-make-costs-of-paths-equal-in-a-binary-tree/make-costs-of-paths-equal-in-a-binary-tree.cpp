94bc2c9bf87151e7c238d84e3ad9d0f818f6056b
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-08-02 13:50:05 +0530

    Time: 1 ms (87.24%) | Memory: 140.5 MB (77.96%) - LeetSync

--- 2780-make-costs-of-paths-equal-in-a-binary-tree/make-costs-of-paths-equal-in-a-binary-tree.cpp
+++ 2780-make-costs-of-paths-equal-in-a-binary-tree/make-costs-of-paths-equal-in-a-binary-tree.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int ans=0;
     int dfs(int root, vector<int>& cost,int n){
-        if(root>n) return 0;
-        int t1 = dfs(root*2,cost,n);
-        int t2 = dfs(root*2+1,cost,n);
+        if(root>=n) return 0;
+        int t1 = dfs(root*2+1,cost,n);
+        int t2 = dfs(root*2+2,cost,n);
         ans += abs(t1-t2);
-        return cost[root-1] + max(t1,t2);
+        return cost[root] + max(t1,t2);
     }
     int minIncrements(int n, vector<int>& cost) {
-        dfs(1,cost,n);
+        dfs(0,cost,n);
         return ans;
     }
 };
