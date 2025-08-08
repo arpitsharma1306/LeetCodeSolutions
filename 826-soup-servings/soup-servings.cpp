e3f3b7800095cf59c6eba0f5a3f30c8044f89450
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-08-08 14:20:53 +0530

    Time: 132 ms (13.33%) | Memory: 427.8 MB (33.33%) - LeetSync

--- 826-soup-servings/soup-servings.cpp
+++ 826-soup-servings/soup-servings.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     vector<vector<double>>t;
-    vector<pair<int,int>> serves = {{100,0},{75,25},{50,50},{25,75}};
-    double solve(double A, double B){
+    vector<pair<int,int>>serves = {{100,0},{75,25},{50,50},{25,75}};
+    double solve(double A,double B){
         if(A<=0 && B<=0){
             return 0.5;
         }
+        if(B<=0) return 0;
         if(A<=0) return 1;
-        if(B<=0) return 0.0;
 
-        if(t[A][B]!=-1.0){
-            return t[A][B];
-        }
-        double prob=0.0;
+        if(t[A][B]!=-1) return t[A][B];
+
+        double prob = 0;
         for(auto P:serves){
             double A_taken=P.first;
             double B_taken=P.second;
             prob += solve(A-A_taken,B-B_taken);
         }
 
-        return t[A][B]=0.25*prob;
+        return t[A][B] = 0.25*prob;
     }
     double soupServings(int n) {
-        if(n>=4300) return 1;
+        if(n>=4400) return 1;
         t.resize(n+1,vector<double>(n+1,-1.0));
         return solve(n,n);
     }
