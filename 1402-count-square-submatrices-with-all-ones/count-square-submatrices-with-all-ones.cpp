cb9de7b486f3dafbeb0200668a235f7b48e760f0
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-08-20 15:15:42 +0530

    Time: 10 ms (32.18%) | Memory: 30.4 MB (48.39%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
+    int m,n;
+
+    bool valid(int x,int y){
+        return x-1>=0 && y-1>=0;
+    }
+
     int countSquares(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-
-        vector<vector<int>>t(m,vector<int>(n,0));
-
-        int result=0;
-
+        m=matrix.size(), n=matrix[0].size();
+        vector<vector<int>>res(m,vector<int>(n));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-
-                if(i==0 || j==0){
-                    t[i][j]=matrix[i][j];
+                if(valid(i,j) && matrix[i][j]==1){
+                    res[i][j] = min({res[i-1][j-1],res[i][j-1],res[i-1][j]})==0?matrix[i][j]:min({res[i-1][j-1],res[i][j-1],res[i-1][j]})+1;
+                }else{
+                    res[i][j] = matrix[i][j];
                 }
-                else if(matrix[i][j]==1){
-                    t[i][j]=1+min({t[i-1][j],t[i-1][j-1],t[i][j-1]});
-                }
-                result+=t[i][j];
             }
         }
 
-        return result;
+        int ans = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans += res[i][j];
+            }
+        }
+
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<res[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+
+        return ans;
     }
 };
