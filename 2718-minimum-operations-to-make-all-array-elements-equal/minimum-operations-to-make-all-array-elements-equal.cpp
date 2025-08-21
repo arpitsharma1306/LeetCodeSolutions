bdbe0bcc1d6d68fc9f1c6be507219bed25e75eae
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-08-21 16:46:58 +0530

    Time: 154 ms (78.83%) | Memory: 86.3 MB (55.23%) - LeetSync

---
+++ 2718-minimum-operations-to-make-all-array-elements-equal/minimum-operations-to-make-all-array-elements-equal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    vector<long long> minOperations(vector<int>& nums, vector<int>& queries) {
+        int n=nums.size();
+        vector<long long>ans;
+        vector<long long>prefix(n+1);
+        prefix[0]=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            prefix[i+1]=prefix[i]+nums[i];
+        }
+
+        for(long long q:queries){
+            int idx = lower_bound(nums.begin(),nums.end(),q)-nums.begin();
+            long long oper = 0;
+            if(idx>0){
+                oper += ((long long)(q*idx) - prefix[idx]);
+            }
+            oper += ((prefix[n]-prefix[idx]) - (long long)(q*(n-idx)));
+            ans.push_back(oper);
+        }
+
+        return ans;
+    }
+};
