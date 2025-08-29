b0b567380530a5d4960f2e76b3f46da88ed26830
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-08-29 11:14:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (100.00%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        long long cnt = 0;
-        for(int i=1;i<=n;i++){
-            if(i%2==0){
-                cnt +=(long long)(m+1)/2;
-            }else{
-                cnt +=(long long)(m/2);
-            }
-        }
-
-        return cnt;
+        return (long long)m*n/2;
     }
 };
