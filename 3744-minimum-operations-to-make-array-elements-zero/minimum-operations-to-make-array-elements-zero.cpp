37261c390fd79babb5b4c454fc8a45920de88f22
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-09-06 15:42:17 +0530

    Time: 16 ms (68.12%) | Memory: 183.2 MB (94.20%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    long long expSum4[18]={1};
-    long long expSum(unsigned x){
-        if (x==0) return 0;
-        int log4=(31-countl_zero(x))/2;
-        int r=x-(1<<(2*log4));
-        return expSum4[log4]+r*(log4+1LL);
+    long long solve(int l,int r){
+        long long L=1;
+        long long S=1;
+        long long steps = 0;
+        
+        while(L<=r){
+            long long R=4*L-1;
+            long long start = max(L,(long long)l);
+            long long end = min(R,(long long)r);
+            if(start<=end){
+                steps += (end-start+1)*S;
+            }
+            S++;
+            L*=4;
+        }
+
+        return steps;
+
     }
     long long minOperations(vector<vector<int>>& queries) {
-        for(int i=1; i<18; i++){
-            expSum4[i]=expSum4[i-1]+3LL*i*(1LL<<(2*(i-1)))+1;
-        //    cout<<i<<"->"<<expSum4[i]<<", ";
+        long long result = 0;
+        for(auto&q:queries){
+            int l=q[0], r=q[1];
+            long long steps = solve(l,r);
+            result += (steps+1)/2;
         }
-        long long op=0;
-        for(auto& q: queries){
-            int l=q[0]-1, r=q[1];
-            op+=(expSum(r)-expSum(l)+1)/2;// ceiling of (expSum(r)-expSum(l))/2
-        }
-        return op;
+        return result;
     }
 };
