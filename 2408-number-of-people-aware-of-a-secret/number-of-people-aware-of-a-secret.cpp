8dd432d1f4f62eef4658463cfec0ee5058dc60f5
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-09-10 20:27:23 +0530

    Time: 23 ms (13.30%) | Memory: 9.8 MB (25.06%) - LeetSync

--- 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
+    const int mod = 1e9+7;
     int peopleAwareOfSecret(int n, int delay, int forget) {
-        vector<long long> dp(n + 1, 0);
-        dp[1] = 1;
-        long long share = 0, MOD = 1000000007;
-        for (int t = 2; t <= n; t++) {
-            if (t - delay > 0)
-                share = (share + dp[t - delay] + MOD) % MOD;
-            if (t - forget > 0)
-                share = (share - dp[t - forget] + MOD) % MOD;
-            dp[t] = share;
+        vector<int>delays(delay,0);
+        vector<int>forgets(forget,0);
+        long long count = 1;
+        long long active = 0;
+        delays[0] = 1;
+        forgets[0] = 1; 
+        for(int i=1;i<n;i++){
+            active = (active + delays[delay-1])%mod;
+            active = (active - forgets[forget-1] + mod)%mod;
+            count = (count + active)%mod;
+            count = (count - forgets[forget-1] + mod)%mod;
+            for(int i=delay-1;i>0;i--){
+                delays[i]=delays[i-1];
+            }
+            delays[0]=active;
+            for(int i=forget-1;i>0;i--){
+                forgets[i]=forgets[i-1];
+            }
+            forgets[0]=active;
         }
-        long long know = 0;
-        for (int i = n - forget + 1; i <= n; i++)
-            know = (know + dp[i]) % MOD;
-        return (int)know;
+
+        return count;
     }
 };
