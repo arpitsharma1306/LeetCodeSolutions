b87dc236672dd4aa63e1f05de3d35f3a62dacaea
Arpit Sharma <128173263+arpitsharma1306@users.noreply.github.com>
2025-09-15 12:43:14 +0530

    Time: 3 ms (18.94%) | Memory: 10.1 MB (5.65%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    int canBeTypedWords(string text, string broken) {
-        int mask = 0;
-        for (int i = 0; i < broken.size(); i++)
-            mask |= 1 << (broken[i] - 97);
-        
+    int canBeTypedWords(string text, string broken){
+        unordered_set<char> brokenKeys;
         int count = 0;
-        int wordMask = 0;
-        for (int i = 0; i <= text.size(); i++) {
-            if (i < text.size() && text[i] >= 'a' && text[i] <= 'z')
-                wordMask |= mask & (1 << (text[i] - 97));
-            if (i == text.size() || text[i] == ' ') {
-                if (wordMask == 0) count++;
-                wordMask = 0;
+
+        for (auto& c : broken){
+            brokenKeys.insert(c);
+        }
+
+        stringstream ss(text);
+        string word;
+        vector<string> words;
+
+        while (ss >> word){
+            words.push_back(word);
+        }
+
+        for (auto w : words)
+        {
+            for (char c : w)
+            {
+                if (brokenKeys.find(c) != brokenKeys.end())
+                {
+                    count++;
+                    break;
+                }
             }
         }
-        return count;
+
+        return words.size() - count;
     }
 };
